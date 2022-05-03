5c053a0ba56100630036fa8243480e44c9f0bcb7
Sofbog <Devilmaycrytrav1@gmail.com>
2022-05-03 12:57:50 +0300

    add static root

--- My_test_CRM/settings.py
+++ My_test_CRM/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #'static_root'
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 AUTH_USER_MODEL = 'leads.User'
